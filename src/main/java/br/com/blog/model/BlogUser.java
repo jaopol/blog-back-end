d87602424a73ae06311fcaf552f7dc91329ac046
jaopol <jaopol@gmail.com>
2020-04-06 13:39:33 -0300

    Refactor BlogUser

--- src/main/java/br/com/blog/model/BlogUser.java
+++ src/main/java/br/com/blog/model/BlogUser.java
@@ -2,15 +2,12 @@ package br.com.blog.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -39,6 +36,7 @@ public class BlogUser implements Serializable {
 	@Column( name = "name", nullable = false, length = 100 )
 	private String name;
 	
+	
 	@Column( name = "login", nullable = false )
 	private String login;
 	
@@ -49,11 +47,6 @@ public class BlogUser implements Serializable {
 	@Column( name = "date_created", nullable = false )
 	private Date createdDate;
 	
-	@OneToMany( mappedBy = "blogUser", cascade = CascadeType.ALL )
-	private List<UserPost> listPost;
-	
-	@OneToMany( mappedBy = "blogUser", cascade = CascadeType.ALL )
-	private List<Commentary> listCommentary;
 	
 
 }

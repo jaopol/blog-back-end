81dbb870173cab2ac891eacc9118a810fac77e97
jaopol <jaopol@gmail.com>
2020-04-07 16:16:39 -0300

    Ajustando configurações para inserir um usuário desde o primeiro acesso

--- src/main/java/br/com/blog/controllers/UsersController.java
+++ src/main/java/br/com/blog/controllers/UsersController.java
@@ -20,7 +20,7 @@ import io.swagger.annotations.ApiOperation;
 
 @Api( value = "API blog - User")
 @RestController
-@RequestMapping( "/api/v1/user" )
+@RequestMapping( "/users" )
 public class UsersController {
 	
 	@Autowired
